0b13943fbfeeeef3711eafbda9c8e93b66291115
Olivia Winteringham <owinteringham@gmail.com>
2019-04-12 17:30:04 +0100

    google analytics and React Portal combined

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReactDOM from "react-dom";
+import Modal from './Modal'
+
+const Outsider = () =>
+  ReactDOM.createPortal(<div>I'm outside!!</div>, document.body);
+
 
 class App extends Component {
-  render() {
-    return (
+  state = {
+    showModal: false
+  };
+  toggleModal = () =>
+    this.setState({
+      showModal: !this.state.showModal
+    });
+  render = () => (
+    <>
+      <Modal open={this.state.showModal} oncClose={this.toggleModal}>
+        This Modal
+      </Modal>
+      <button onClick={this.toggleModal}>Show Modal??????</button>
       <div className="App">
+        <Outsider />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -21,8 +39,23 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
-  }
+    </>
+  );
 }
 
 export default App;
+
+// constructor(props){
+//   super(props)
+//   ReactGA.event({
+//     category: "person on my site",
+//     action: "clicked the react link"
+//   });
+// }
+
+// import ReactGA from "react-ga";
+
+// function initializeReactGA() {
+//   ReactGA.initialize("UA-138250788-1");
+//   ReactGA.pageview("/homepage");
+// }
